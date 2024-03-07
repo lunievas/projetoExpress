396ba96637aa39b4b03740be92952488f652850c
Luisa Nievas <luisa.nievas94@gmail.com>
2024-03-07 11:22:47 -0300

    Feat: add MongoUser protocol

--- src/repositories/delete-user/mongo-delete-user.ts
+++ src/repositories/delete-user/mongo-delete-user.ts
@@ -2,11 +2,12 @@ import { ObjectId } from "mongodb";
 import { IDeleteUserRepository } from "../../controllers/delete-user/protocols";
 import { User } from "../../models/user";
 import { MongoClient } from "../../database/mongo";
+import { MongoUser } from "../mongo-protocols";
 
 export class MongoDeleteUserRepository implements IDeleteUserRepository {
   async deleteUser(id: string): Promise<User> {
     const user = await MongoClient.db
-      .collection<Omit<User, "id">>("users")
+      .collection<MongoUser>("users")
       .findOne({ _id: new ObjectId(id) });
 
     if (!user) {
